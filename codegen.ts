b9b6c71e3a605f14d2214a4e1bd641891a20aadd
Peter Schilling <peter@schpet.com>
2025-08-11 21:42:39 -0700

    codegen setup

---
+++ codegen.ts
@@ -0,0 +1,17 @@
+import type { CodegenConfig } from "@graphql-codegen/cli";
+
+const config: CodegenConfig = {
+  schema: "https://api.linear.app/graphql",
+  documents: ["main.ts"],
+  generates: {
+    "generated/": {
+      preset: "client",
+      plugins: [],
+      config: {
+        enumsAsTypes: true,
+      },
+    },
+  },
+};
+
+export default config;
